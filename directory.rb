4559c3d8375ed583a5ab82a2ffc627c807571f49
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-11 14:00:58 +0100

    Added interactive menu - first draft

--- directory.rb
+++ directory.rb
@@ -1,3 +1,48 @@
+#Print the students' names
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students, poss_cohorts)
+  students.sort_by{|student| poss_cohorts.index(student[:cohort].to_s)}.each do |student|
+    puts "#{student[:name]}".center(30) + "#{student[:cohort]} cohort".center(18) #+ "#{student[:height]} tall and enjoys #{student[:hobbies]}".center(35)
+  end
+end
+
+def print_footer(names)
+  puts "Overall we have #{names.count} great student" + (names.count > 1 ? "s" : "")
+end
+
+def interactive_menu(poss_cohorts)
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input students"
+    puts "2. Show student data"
+    puts "9. Exit"
+    # 2. read the input and save to a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students poss_cohorts
+    when "2"
+      # if students.length > 0
+        print_header
+        print(students, poss_cohorts)
+        print_footer(students)
+      # else
+      #   puts "No student data to show"
+      # end
+    when "9"
+      exit
+    else
+      puts "Sorry, please enter a valid option from the list"
+    end
+  end
+end
+
 poss_cohorts = ["september", "october", "november", "december"]
 
 def input_students(poss_cohorts)
@@ -25,9 +70,8 @@ def input_students(poss_cohorts)
   students
 end
 
-
-#Get the user to enter the names of the students
-students = input_students poss_cohorts
+# open interactive menu and find out what user wants to do
+interactive_menu poss_cohorts
 # students = [
 #   {name: "Dr. Hannibal Lecter", cohort: poss_cohorts.sample.to_sym, hobbies: "football", height: "182cm"},
 #   {name: "Darth Vader", cohort: poss_cohorts.sample.to_sym, hobbies: "homebrewing", height: "187cm"},
@@ -41,25 +85,3 @@ students = input_students poss_cohorts
 #   {name: "Joffrey Baratheon", cohort: poss_cohorts.sample.to_sym, hobbies: "coding", height: "165cm"},
 #   {name: "Norman Bates", cohort: poss_cohorts.sample.to_sym, hobbies: "music", height: "182cm"}
 # ]
-
-#Print the students' names
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-
-def print(students, poss_cohorts)
-  students.sort_by{|student| poss_cohorts.index(student[:cohort].to_s)}.each do |student|
-    puts "#{student[:name]}".center(30) + "#{student[:cohort]} cohort".center(18) #+ "#{student[:height]} tall and enjoys #{student[:hobbies]}".center(35)
-  end
-end
-
-def print_footer(names)
-  puts "Overall we have #{names.count} great student" + (names.count > 1 ? "s" : "")
-end
-
-if students.length > 0
-  print_header
-  print(students, poss_cohorts)
-  print_footer(students)
-end
