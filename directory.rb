149e33e4789ff3cbe0e87939d211d94b0a8f268c
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-11 12:50:40 +0100

    Ex 7: added cohort to user input with basic error handling

--- directory.rb
+++ directory.rb
@@ -7,7 +7,16 @@ def input_students
   name = gets.chomp
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
+    poss_cohorts = ["september", "october", "november", "december"]
+    cohort = "initial value highly unlikely anyone will type this in"
+    while !poss_cohorts.include?(cohort)
+      if cohort != "initial value highly unlikely anyone will type this in"
+        puts "Sorry, don't recognise that as a cohort. Possible values are: " + poss_cohorts.join(", ")
+      end
+      puts "Which cohort is #{name} heading for?"
+      cohort = gets.chomp
+    end
+    students << {name: name, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
@@ -17,20 +26,20 @@ end
 
 
 #Get the user to enter the names of the students
-# students = input_students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "football", height: "182cm"},
-  {name: "Darth Vader", cohort: :november, hobbies: "homebrewing", height: "187cm"},
-  {name: "Nurse Ratched", cohort: :november, hobbies: "football", height: "168cm"},
-  {name: "Michael Corleone", cohort: :november, hobbies: "fishing", height: "166cm"},
-  {name: "Alex DeLarge", cohort: :november, hobbies: "football", height: "160cm"},
-  {name: "The Wicked Witch of the West", cohort: :november, hobbies: "fishing", height: "174cm"},
-  {name: "Terminator", cohort: :november, hobbies: "football", height: "189cm"},
-  {name: "Freddy Krueger", cohort: :november, hobbies: "homebrewing", height: "175cm"},
-  {name: "The Joker", cohort: :november, hobbies: "coding", height: "171cm"},
-  {name: "Joffrey Baratheon", cohort: :november, hobbies: "coding", height: "165cm"},
-  {name: "Norman Bates", cohort: :november, hobbies: "music", height: "182cm"}
-]
+students = input_students
+# students = [
+#   {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "football", height: "182cm"},
+#   {name: "Darth Vader", cohort: :november, hobbies: "homebrewing", height: "187cm"},
+#   {name: "Nurse Ratched", cohort: :november, hobbies: "football", height: "168cm"},
+#   {name: "Michael Corleone", cohort: :november, hobbies: "fishing", height: "166cm"},
+#   {name: "Alex DeLarge", cohort: :november, hobbies: "football", height: "160cm"},
+#   {name: "The Wicked Witch of the West", cohort: :november, hobbies: "fishing", height: "174cm"},
+#   {name: "Terminator", cohort: :november, hobbies: "football", height: "189cm"},
+#   {name: "Freddy Krueger", cohort: :november, hobbies: "homebrewing", height: "175cm"},
+#   {name: "The Joker", cohort: :november, hobbies: "coding", height: "171cm"},
+#   {name: "Joffrey Baratheon", cohort: :november, hobbies: "coding", height: "165cm"},
+#   {name: "Norman Bates", cohort: :november, hobbies: "music", height: "182cm"}
+# ]
 
 #Print the students' names
 def print_header
@@ -40,7 +49,7 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]}".center(30) + "#{student[:cohort]} cohort".center(18) + "#{student[:height]} tall and enjoys #{student[:hobbies]}".center(35)
+    puts "#{student[:name]}".center(30) + "#{student[:cohort]} cohort".center(18) #+ "#{student[:height]} tall and enjoys #{student[:hobbies]}".center(35)
   end
 end
 
