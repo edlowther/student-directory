5bf8be80cf986c849a0ff4d2b7035197b100340e
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-11 15:57:20 +0100

    Refactored file.open statements

--- directory.rb
+++ directory.rb
@@ -97,13 +97,13 @@ def interactive_menu
 end
 
 def save_students(filename = "students.csv")
-  file = File.open(filename, 'w')
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  File.open(filename, 'w') do |file|
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
 end
 
 def try_load_students
@@ -124,12 +124,12 @@ def add_student(name, cohort)
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(",")
-    add_student(name, cohort)
+  File.open(filename, "r") do |file|
+    file.readlines.each do |line|
+      name, cohort = line.chomp.split(",")
+      add_student(name, cohort)
+    end
   end
-  file.close
 end
 
 def get_cohort name
