148d00341a335111d512189a920f21db2f249583
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-11 09:18:11 +0100

    Added number of students in directory

---
+++ directory.rb
@@ -0,0 +1,19 @@
+#First we print the list of students
+puts "The students of Villains Academy"
+puts "-------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+#Finally we print the total number of students
+print "Overall we have "
+#It's important that print() doesn't add new line characters
+print 11
+print " great students"
