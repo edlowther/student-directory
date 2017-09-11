16b8baf16a7977848a6b969f77454b5355365e92
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-11 10:55:55 +0100

    Ex 5: Now prints data on hobbies and heights too

--- directory.rb
+++ directory.rb
@@ -40,7 +40,7 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort) - is #{student[:height]} tall and enjoys #{student[:hobbies]}"
   end
 end
 
