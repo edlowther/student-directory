5d90772235017a1538bd4c4f2d31958b7a2f3545
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-11 12:41:37 +0100

    Ex 6: formatted text

--- directory.rb
+++ directory.rb
@@ -40,7 +40,7 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort) - is #{student[:height]} tall and enjoys #{student[:hobbies]}"
+    puts "#{student[:name]}".center(30) + "#{student[:cohort]} cohort".center(18) + "#{student[:height]} tall and enjoys #{student[:hobbies]}".center(35)
   end
 end
 
